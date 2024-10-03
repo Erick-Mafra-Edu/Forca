ed4b418a6eac210ef8004f3bfdc3387a8e683a24
Erick Marlon Mafra <erick.8315728@edu.univali.br>
2024-10-03 20:14:49 -0300

    ABROBRINHA

--- main.cpp
+++ main.cpp
@@ -1,1 +1,27 @@
 #include<iostream>
+#include<locale.h>
+using namespace std;
+
+int main(){
+    setlocale(LC_ALL,"Portuguese");
+    int key;
+    cout<<"comecar";
+    cin>>key;
+    char letra;
+    int seletor;
+    char L1, L2, L3, L4, L5, L6;
+    do{
+        switch(key){
+            case 1:
+                do{
+                cin>>letra;
+                }while(ERRO != 10 || ACERTO != 10)
+            break;
+            case 2:
+            break;
+            default:
+            cout<< "Opção inválida";
+        }
+    }while(key == 3)
+    return 0;
+}
